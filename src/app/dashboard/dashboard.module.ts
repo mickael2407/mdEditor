db4c0ef1f6170c7b465cec03e11c66c377c536d8
mickael2407 <pezzoni.mickael@gmail.com>
2020-01-28 17:01:42 +0100

    editor css

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -21,7 +21,7 @@ import { RouterModule } from '@angular/router';
     RouterModule,
     ReactiveFormsModule,
     NgxMdModule.forRoot(),
-    DashboardRoutingModule
+    DashboardRoutingModule,
   ],
   providers: [
     DocsService,
