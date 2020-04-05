6232beaff0b6777a428c692e39a788b251de6da7
Mickael <pezzoni.mickael@gmail.com>
2020-04-05 19:52:48 +0200

    change view process

--- src/app/dashboard/view/view.component.ts
+++ src/app/dashboard/view/view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DocsService } from 'src/app/service/docs.service';
 import { Doc } from 'src/app/interface/doc';
 
@@ -11,9 +11,21 @@ import { Doc } from 'src/app/interface/doc';
 export class ViewComponent implements OnInit {
 
   public currentDoc: Doc;
-  constructor(private activatedRoute: ActivatedRoute, public docService: DocsService) {
+  constructor(private activatedRoute: ActivatedRoute, 
+    public docService: DocsService,
+    private router: Router,
+    private routerSnapshot: ActivatedRoute) {
     this.currentDoc = this.docService.allDocs.filter(_doc => _doc._id == this.activatedRoute.snapshot.params['docId'])[0];
     this.loadContent(this.currentDoc._id);
+    this.routerSnapshot.params.subscribe(
+      _params => {
+        if (_params['docId'] !== undefined) {
+          this.currentDoc = this.docService.allDocs.filter(_doc => _doc._id == _params['docId'])[0];
+          this.loadContent(this.currentDoc._id);
+          
+        }
+      }
+    )
   }
 
   ngOnInit() {
@@ -33,16 +45,15 @@ export class ViewComponent implements OnInit {
   nextDoc(): void {
     const currentIndex = this.docService.allDocs.findIndex(_doc => _doc._id === this.currentDoc._id);
     if (currentIndex !== this.docService.allDocs.length - 1) {
-      this.currentDoc = this.docService.allDocs[currentIndex + 1];
-      this.loadContent(this.currentDoc._id);
+        this.router.navigate(['dashboard/view', this.docService.allDocs[currentIndex + 1]._id])
     }
   }
 
   previousDoc(): void {
     const currentIndex = this.docService.allDocs.findIndex(_doc => _doc._id === this.currentDoc._id);
     if (currentIndex !== 0) {
-      this.currentDoc = this.docService.allDocs[currentIndex - 1];
-      this.loadContent(this.currentDoc._id);
+      this.router.navigate(['dashboard/view', this.docService.allDocs[currentIndex - 1]._id])
+
     } 
   }
 
